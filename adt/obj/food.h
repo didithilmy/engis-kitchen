7fb71a610b971b954ad732c326c644673c04d446
Audi <maharanic20@gmail.com>
2018-11-20 16:47:15 +0700

    dari audi

--- adt/obj/food.h
+++ adt/obj/food.h
@@ -1,13 +1,30 @@
 /**
  * Food adt
  */
+ 
+#define MAX_FOOD 20
+
 #ifndef ENGIS_KITCHEN_FOOD_H
 #define ENGIS_KITCHEN_FOOD_H
 
 #include "../mesin_kata.h"
+#include "../point.h"
 
 typedef struct {
+	POINT coordinate;
     Kata name;
 } Food;
+
+typedef struct {
+    Food T[MAX_FOOD + 1];
+    int N;
+} TabFood;
+
+void AddFood(TabFood *TF, Food F);
+//I.S adalah Tabel Food sembarang 
+//F.S adalah makanan ditambahkan dalam tabel makanan 
+
+Food CreateFood(POINT coord, Kata nama); 
+//Fungsi pada awal loading file untuk menambahkan makanan dengan koordinatnya
 
 #endif //ENGIS_KITCHEN_FOOD_H
