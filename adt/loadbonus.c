e57fd6b690cd5376c748b38d4371e4ec5ab743da
Audi <maharanic20@gmail.com>
2018-11-27 20:30:47 +0700

    DIDIT SUDAH KU BUAT YANG BONUS tapi gatau bener apa ga HEHE

---
+++ adt/loadbonus.c
@@ -0,0 +1,157 @@
+
+#include "headers.h"
+#include <stdlib.h>
+#include <stdio.h> 
+
+int katatoint (Kata kata); 
+
+Kata nama;
+int money, life, time;  
+
+typedef struct {
+		int capacity;
+		int patience;
+	} typequeue;
+	
+typequeue queue[10] ; 
+
+char * stack[10]; 
+
+typedef struct {
+		int capacity;
+		int isi;
+		int patience;
+		Kata * makanan;
+	} typemeja;
+	
+typemeja meja[10]; 
+
+int i ; 
+
+int main () { 
+	
+	STARTFILE("fileeks.txt"); 
+	while (!IsEOPF())
+    {
+        if (GetFCC() != ';')
+        {
+			if (GetFCC() == '(') {
+			ADVFILE(); 
+			MKF_SalinKata();
+			nama = MKF_CKata; 
+			}
+            ADVFILE();
+            if (GetFCC() == ')')
+            {
+                ADVFILE();
+            }
+        }
+        ADVFILE();
+        if (GetFCC() != ';') {
+			if (GetFCC() == '(') {
+			ADVFILE(); 
+			MKF_SalinKata();
+			money = katatoint(MKF_CKata); 
+			}
+            ADVFILE();
+            if (GetFCC() == ')')
+            {
+                ADVFILE();
+            }
+		}
+		ADVFILE(); 
+		if (GetFCC() != ';') {
+			if (GetFCC() == '(') {
+			ADVFILE(); 
+			MKF_SalinKata();
+			life = katatoint(MKF_CKata); 
+			}
+            ADVFILE();
+            if (GetFCC() == ')')
+            {
+                ADVFILE();
+            }
+		}
+		ADVFILE(); 
+		if (GetFCC() != ';') {
+			if (GetFCC() == '(') {
+			ADVFILE(); 
+			MKF_SalinKata();
+			time = katatoint(MKF_CKata); 
+			}
+            ADVFILE();
+            if (GetFCC() == ')')
+            {
+                ADVFILE();
+            }
+		}
+		ADVFILE(); 
+		i=1; 
+		while (GetFCC() != ';') {
+			if (GetFCC() == '(') {
+			ADVFILE(); 
+			MKF_SalinKata();
+			queue[i].capacity= katatoint(MKF_CKata);
+			MKF_ADVKATA(); 
+			queue[i].patience = katatoint(MKF_CKata);
+			i++; 
+			}
+            ADVFILE();
+            if (GetFCC() == ')')
+            {
+                ADVFILE();
+            }
+		}
+		ADVFILE(); 
+		i=1; 
+		while (GetFCC() != ';') { 
+			if (GetFCC() == '(') {
+			ADVFILE(); 
+			MKF_SalinKata();
+			stack[i]= katatoint(MKF_CKata);
+			i++; 
+			}
+			ADVFILE(); 
+			if (GetFCC() == ')')
+            {
+                ADVFILE();
+            }
+		}
+		ADVFILE(); 
+		i=1; 
+		while (GetFCC() != '.') {
+			if (GetFCC() == '(') {
+			ADVFILE(); 
+			MKF_SalinKata();
+			meja[i].capacity= katatoint(MKF_CKata);
+			MKF_ADVKATA(); 
+			meja[i].isi = katatoint(MKF_CKata);
+			MKF_ADVKATA(); 
+			meja[i].makanan = &MKF_CKata;  
+			i++; 
+			}
+            ADVFILE();
+            if (GetFCC() == ')')
+            {
+                ADVFILE();
+            }
+		}
+	}
+
+}
+ 
+
+
+/**
+ * Convert Kata to integer
+ */
+int katatoint (Kata kata) {
+	int i;
+	int total = 0;
+	
+	for (i=1;i<=kata.Length;i++) {
+		total *= 10;
+		total += kata.TabKata[i]-'0';
+	}
+	return total;
+}
