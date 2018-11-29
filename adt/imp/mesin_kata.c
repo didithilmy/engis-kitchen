f09832339bce252cc49366255f121c9b5c3abe49
yanzahatta <yanza.hatta@gmail.com>
2018-11-29 18:19:54 +0700

    komentar pada adt

--- adt/imp/mesin_kata.c
+++ adt/imp/mesin_kata.c
@@ -13,13 +13,23 @@
 boolean EndKata;
 Kata CKata;
 
+
+/*{ Mengabaikan satu atau beberapa BLANK }
+    { I.S. : CC sembarang }
+    { F.S. : CC = BLANK atau CC = MARK }*/
 void Ignore_Blank() {
+
     while(GetCC() == BLANK && (GetCC() != MARK)) {
         ADV();
     }
 }
 
+/*{ I.S. : CC sembarang, filename merupakan string nama file pita kata yang akan dibaca }
+{ F.S. : EndKata = true, dan CC = Mark; }
+{        atau EndKata = false, CKata adalah kata yang sudah diakuisisi,
+         CC karakter pertama sesudah karakter terakhir kata }*/
 void STARTKATA(char *string) {
+
     START(string);
     Ignore_Blank();
 
@@ -32,6 +42,11 @@ void STARTKATA(char *string) {
     }
 }
 
+/*{ I.S. : EndKata = false; CC adalah karakter sesudah karakter terakhir
+         dari kata yg sudah diakuisisi }
+{ F.S. : Jika CC = MARK, maka EndKata = true
+         atau EndKata = false, CKata adalah kata terakhir yang sudah diakuisisi;
+         CC karakter pertama sesudah karakter terakhir kata }*/
 void ADVKATA() {
     Ignore_Blank();
     if(IsEOP()) {
@@ -41,6 +56,11 @@ void ADVKATA() {
     }
 }
 
+/*{ Mengakuisisi kata, menyimpan dalam CKata }
+{ I.S. : CC adalah karakter pertama dari kata }
+{ F.S. : CKata berisi kata yang sudah diakuisisi, jika karakternya melebihi
+         NMax, sisa "kata" dibuang; CC = BLANK atau CC = MARK; CC adalah
+         karakter sesudah karakter terakhir yang diakuisisi }*/
 void SalinKata() {
     int i = 1;
     while(1) {
@@ -55,6 +75,12 @@ void SalinKata() {
     CKata.Length = i;
 }
 
+
+/**
+ * Build Kata from string
+ * @param str string
+ * @return Kata
+ */
 Kata BuildKata(char *str) {
     int i;
 
--- adt/imp/mesin_kata_file.c
+++ adt/imp/mesin_kata_file.c
@@ -3,6 +3,7 @@
  * Mesin Kata abstraction
  *
  * @author Muhammad Aditya Hilmy, NIM 18217025
+ * @author Muhammad Yanza Hattari, NIM 18217043
  * @file mesin_kata_file.c
  */
 
@@ -13,12 +14,19 @@
 boolean MKF_EndKata;
 Kata MKF_CKata;
 
+/*{ Mengabaikan satu atau beberapa BLANK }
+{ I.S. : FCC sembarang }
+{ F.S. : FCC = BLANK atau FCC = MARK }*/
 void MKF_Ignore_Blank() {
     while(GetFCC() == BLANK && (GetFCC() != MKF_MARK)) {
         ADVFILE();
     }
 }
 
+/*{ I.S. : FCC sembarang, filename merupakan string nama file pita kata yang akan dibaca }
+{ F.S. : EndKata = true, dan FCC = Mark; }
+{        atau EndKata = false, CKata adalah kata yang sudah diakuisisi,
+         FCC karakter pertama sesudah karakter terakhir kata }*/
 void MKF_STARTKATA(char *string) {
     STARTFILE(string);
     MKF_Ignore_Blank();
@@ -31,6 +39,11 @@ void MKF_STARTKATA(char *string) {
     }
 }
 
+/*{ I.S. : EndKata = false; FCC adalah karakter sesudah karakter terakhir
+         dari kata yg sudah diakuisisi }
+{ F.S. : Jika FCC = MARK, maka EndKata = true
+         atau EndKata = false, MKF_CKata adalah kata terakhir yang sudah diakuisisi;
+         FCC karakter pertama sesudah karakter terakhir kata }*/
 void MKF_ADVKATA() {
     MKF_Ignore_Blank();
     if(IsEOPF()) {
@@ -40,6 +53,11 @@ void MKF_ADVKATA() {
     }
 }
 
+/*{ Mengakuisisi kata, menyimpan dalam MKF_CKata }
+{ I.S. : FCC adalah karakter pertama dari kata }
+{ F.S. : MKF_CKata berisi kata yang sudah diakuisisi, jika karakternya melebihi
+         NMax, sisa "kata" dibuang; FCC = BLANK atau FCC = MARK; FCC adalah
+         karakter sesudah karakter terakhir yang diakuisisi }*/
 void MKF_SalinKata() {
     int i = 1;
     while(1) {
